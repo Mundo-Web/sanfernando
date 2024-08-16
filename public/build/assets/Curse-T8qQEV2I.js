b371d983e4bac7df4ae258d1eaf086b9888a3b3e
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-08-16 18:00:50 -0500

    Agregando modulo de modulos

---
+++ public/build/assets/Curse-T8qQEV2I.js
@@ -0,0 +1,1 @@
+import"./HtmlContent-TT1qJ6UJ.js";import"./ProductCard-0xAfiOtl.js";import"./_commonjsHelpers-4gQjN7DL.js";
--- resources/js/CursoDetalle.jsx
+++ resources/js/CursoDetalle.jsx
@@ -1,14 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import CreateReactScript from './Utils/CreateReactScript'
-import { Fetch } from 'sode-extend-react'
-import arrayJoin from './Utils/ArrayJoin'
-import { set } from 'sode-extend-react/sources/cookies'
-import SliderFront from './components/Section/SliderFront'
-import SliderBenefit from './components/Section/SliderBenefit'
-import TwoColumn from './components/Section/TwoColumn'
-import Curse from './components/Product/Curse'
-import SliderTestimony from './components/Section/SliderTestimony'
 import DropdownComponent from './components/Inputs/DropdownComponent'
 import { format, parseISO } from 'date-fns';
 import { es } from 'date-fns/locale';
