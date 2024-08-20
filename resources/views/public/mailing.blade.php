2cde4c0a5fc5a77690f866f4839a25f0cce4601f
Papayadev2024 <diegomartinez.papaya@gmail.com>
2024-08-20 12:49:59 -0500

    final

---
+++ resources/views/public/mailing.blade.php
@@ -0,0 +1,162 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mundo web</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+      rel="stylesheet"
+    />
+    <style>
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+    </style>
+  </head>
+  <body>
+    <main>
+      <table
+        style="
+          width: 600px;
+          margin: 0 auto;
+          text-align: center;
+          background-image: url(' .
+                $baseUrl .
+                '/fondo.png);
+          background-repeat: no-repeat;
+          background-position: center;
+          background-size: cover;
+        "
+      >
+        <thead>
+          <tr>
+            <th
+              style="
+                display: flex;
+                flex-direction: row;
+                justify-content: center;
+                align-items: center;
+                margin: 40px;
+                padding: 0 200px;
+              "
+            >
+              <a href="' .
+                $baseUrllink .
+                '" target="_blank" style="text-align:center" ><img src="' .
+                $baseUrl .
+                '/logo.png" alt="hpi" /></a>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <p
+                style="
+                  color: #ffffff;
+                  font-weight: 500;
+                  font-size: 40px;
+                  text-align: center;
+                  width: 500px;
+                  margin: 0 auto;
+                  padding: 20px 0;
+                  font-family: Montserrat, sans-serif;
+                "
+              >
+                <span style="display: block">Hola ' .
+                $name .
+                '</span>
+                <span style="display: block">Tienes un nuevo mensaje</span>
+              </p>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <a
+                target="_blank"
+                href="' .
+                $baseUrllink .
+                '"
+                style="
+                  text-decoration: none;
+                  background-color: #fdfefd;
+                  color: #254f9a;
+                  padding: 16px 20px;
+                  display: inline-flex;
+                  justify-content: center;
+                  border-radius: 10px;
+                  align-items: center;
+                  gap: 10px;
+                  font-weight: 600;
+                  font-family: Montserrat, sans-serif;
+                  font-size: 16px;
+                  margin-bottom: 350px;
+                "
+              >
+                <span>Visita nuestra web</span>
+              </a>
+            </td>
+          </tr>
+          <tr style="margin-top: 300px">
+            <td>
+              <a
+                href="' .
+                htmlspecialchars($generales->facebook, ENT_QUOTES, 'UTF-8') .
+                '"
+                target="_blank"
+                style="padding: 0 5px 30px 0; display: inline-block"
+              >
+                <img src="./mail/facebook.png" alt="facebook"
+              /></a>
+
+              <a
+                href="' .
+                htmlspecialchars($generales->instagram, ENT_QUOTES, 'UTF-8') .
+                '"
+                target="_blank"
+                style="padding: 0 5px 30px 0; display: inline-block"
+              >
+                <img src="./mail/instagram.png" alt="instagram"
+              /></a>
+
+              <a
+                href="' .
+                htmlspecialchars($generales->twitter, ENT_QUOTES, 'UTF-8') .
+                '"
+                target="_blank"
+                style="padding: 0 5px 30px 0; display: inline-block"
+              >
+                <img src="./mail/twitter.png" alt="twitter"
+              /></a>
+
+              <a
+                href="' .
+                htmlspecialchars($generales->linkedin, ENT_QUOTES, 'UTF-8') .
+                '"
+                target="_blank"
+                style="padding: 0 5px 30px 0; display: inline-block"
+              >
+                <img src="./mail/linkedin.png" alt="linkedin"
+              /></a>
+
+              <a
+                href="' .
+                htmlspecialchars($generales->youtube, ENT_QUOTES, 'UTF-8') .
+                '"
+                target="_blank"
+                style="padding: 0 5px 30px 0; display: inline-block"
+              >
+                <img src=" ./mail/youtube.png" alt="youtube"
+              /></a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  </body>
+</html>
