ebbc8a4bff07888e94d62deb162ff4f674a622ae
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-09-16 17:25:03 -0500

    Agregando elipsis

--- resources/js/components/Courses/CourseCard.jsx
+++ resources/js/components/Courses/CourseCard.jsx
@@ -6,7 +6,7 @@ const CourseCard = ({ imagen, producto, extract, description, hasShadow = false,
   return <Tippy content={producto}>
     <button className={`w-full max-w-sm relative inline-flex items-center text-sm text-gray-500 hover:text-gray-900 focus:outline-none dark:hover:text-white dark:text-gray-400 bg-white px-4 py-3 rounded-md ${hasShadow && 'shadow-md'}`} {...props}>
       <div className="flex-shrink-0">
-        <img className="rounded-md w-11 h-11" src={`/${imagen}`} onError={e => e.target.src = '/images/img/noimagen.jpg'} alt={producto} />
+        <img className="rounded-md w-11 h-11 object-cover object-center" src={`/${imagen}`} onError={e => e.target.src = '/images/img/noimagen.jpg'} alt={producto} />
       </div>
       <div className="w-full ps-3 text-start">
         <h4 className='w-[calc(100%-40px)] font-semibold text-ellipsis text-nowrap overflow-hidden '>{producto}</h4>
