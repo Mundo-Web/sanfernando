034133229ec43c9e2d0eecb96f7ac91ee40decd2
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-02-26 18:33:47 -0500

    final

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
-            $table->string('avatar')->nullable();
+            $table->text('avatar')->nullable();
             $table->string('external_id')->nullable();
             $table->string('external_auth')->nullable();
             $table->timestamps();
