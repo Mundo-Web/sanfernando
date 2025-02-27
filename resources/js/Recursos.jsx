937b93f79cf00ace687b8c31c1930f9eac9edd7b
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-02-26 21:03:18 -0500

    final

--- resources/js/Recursos.jsx
+++ resources/js/Recursos.jsx
@@ -130,7 +130,7 @@ const Recursos = ({ recursos, documentoscat, aboutUs }) => {
                     
                     </div>
 
-                    <div className='w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6'>
+                    <div className='w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6'>
                        {items.map((recursos, index) => (<Recurso recurso={recursos} />))}
                     </div>
 
