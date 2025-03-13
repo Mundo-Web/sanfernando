a824e4841b78c7e60f2436951857e63872eade73
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-03-13 10:59:46 -0500

    final

--- resources/views/pages/products/save.blade.php
+++ resources/views/pages/products/save.blade.php
@@ -407,7 +407,7 @@
 
                   </div>
                   <div class="w-full">
-                    <label for="duracion">Duracion
+                    <label for="duracion">Duración (En minutos)
                       <span class="text-red-500 font-bold">*</span>
                     </label>
                     <div class="relative mb-2  mt-2">
@@ -417,7 +417,7 @@
                       <input type="number" id="duracion" name="duracion" value="{{ $product->duracion }}"
                         step="0.1"
                         class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="duracion" required>
+                        placeholder="Duración" required>
                     </div>
 
                   </div>
